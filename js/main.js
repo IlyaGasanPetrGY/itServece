f99e3f984df19507952ec87041b052bb65ed23a5
GasanIlya <ilya.gasan2019@mail.ru>
2021-12-11 17:23:59 +0300

    fz text pt change

--- js/main.js
+++ js/main.js
@@ -85,20 +85,37 @@ leftK.on("click", e => {
     
  });
 var count = 0;
- $(".pt__text__content-before").on("click", e => {
-     console.log("hi")
-     if (count % 2 == 0) {
-        $(".pt__text__content").css("position", "relative")
-        $(".pt__text__content").css("height", "auto")
-        $(".pt__text__content-before").css("background", "inherit");
-        count += 1;
-     }
-     else {
-         console.log (count)
-        $(".pt__text__content").css("position", "absolute")
-        $(".pt__text__content").css("height", "10rem")
-        $(".pt__text__content-before").css("background", "linear-gradient(to bottom, rgba(255, 255, 255, 0), #dce0e0 75%)"); 
-        count += 1;
+$(".pt__el--ilya").find(".pt__text__content-before").on("click", e => {
+    console.log("hi")
+    if (count % 2 == 0) {
+       $(".pt__el--ilya").find(".pt__text__content").css("position", "relative")
+       $(".pt__el--ilya").find(".pt__text__content").css("height", "auto")
+       $(".pt__el--ilya").find(".pt__text__content-before").css("background", "inherit");
+       count += 1;
     }
- })
+    else {
+        console.log (count)
+       $(".pt__el--ilya").find(".pt__text__content").css("position", "absolute")
+       $(".pt__el--ilya").find(".pt__text__content").css("height", "10rem")
+       $(".pt__el--ilya").find(".pt__text__content-before").css("background", "linear-gradient(to bottom, rgba(255, 255, 255, 0), #dce0e0 75%)"); 
+       count += 1;
+   }
+})
   
+ $(".pt__el--kate").find(".pt__text__content-before").on("click", e => {
+    console.log("hi")
+    if (count % 2 == 0) {
+       $(".pt__el--kate").find(".pt__text__content").css("position", "relative")
+       $(".pt__el--kate").find(".pt__text__content").css("height", "auto")
+       $(".pt__el--kate").find(".pt__text__content-before").css("background", "inherit");
+       count += 1;
+    }
+    else {
+        console.log (count)
+       $(".pt__el--kate").find(".pt__text__content").css("position", "absolute")
+       $(".pt__el--kate").find(".pt__text__content").css("height", "10rem")
+       $(".pt__el--kate").find(".pt__text__content-before").css("background", "linear-gradient(to bottom, rgba(255, 255, 255, 0), #dce0e0 75%)"); 
+       count += 1;
+   }
+})
+ 
